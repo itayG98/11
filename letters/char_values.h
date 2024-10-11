46620a2f6b63fc902cbaafdf75c380b98282ccf9
itayG98 <itai.g98@gmail.com>
2024-10-11 19:17:21 +0300

    add char val m macro

---
+++ letters/char_values.h
@@ -0,0 +1,13 @@
+#ifndef CHAR_VALUES_H
+#define CHAR_VALUES_H
+
+#define ZERO_DIGIT 48
+#define NINE_DIGIT 57
+#define SMALL_A 97
+#define CAPITAL_A 65
+#define SMALL_Z 122
+#define CAPITAL_Z 90
+#define QUOTE 34
+#define DOT 46
+
+#endif
--- letters/letters.c
+++ letters/letters.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
+#include "char_values.h"
 
 /**
- * @brief Entry point for BLA BLA BLA.
+ * @brief Entry point for letter program.
  *
- * This function prompts the user for BLA BLA BLA
+ * This function prompts the user for inserting text via stdin (could be file or console) and than manipulate it
+ * Capitalize first letter and quted texts , uncapitlize other letters and remove digits.
  * *
  * Example usage:
  * @code
- *    // Input "lalala"
+ *    // Input:
+ *  I Am young. You are young. All of us are young.
+ * "I think we  need    some help.  please" HELP. NO,   NO  NO,
+ * I DO NOT
+ *      NEED HELP
+ *
+ * WHATSOEVER.
+ * "Todays date is
+ *      15/2/2021"...
+ * I am 18  years old, are you 20 years old? Maybe      30 years?
+ *
+ *   // Output:
+ *  *  I Am young. You are young. All of us are young.
+ * "I THINK WE  NEED    SOME HELP.  PLEASE" help. NO,   no  no,
+ * i do not
+ *      need help
+ *
+ * whatsoever.
+ * "TODAY'S DATE IS
+ *      //"...
+ * I am   years old, are you  years old? maybe      30 years?
  * @endcode
  */
 int main()
 {
-    unsigned int a, b;
-    printf("We are letters\n");
-    printf("Please insert two decimal unsigned number seperated by spaces.\n");
-    scanf("%u  %u", &a, &b);
-    printf("Press any key to continue...\n");
+    int current_char;
+    /* is_in_quote = 0, is_first_letter = 1; */
+    printf("Please insert a text to manipulate.\n\n\n");
+    while ((current_char = getchar()) != EOF)
+    {
+        if (current_char >= ZERO_DIGIT && current_char <= NINE_DIGIT)
+        {
+            continue;
+        }
+
+        printf("%c", current_char);
+    }
     return 1;
 }
