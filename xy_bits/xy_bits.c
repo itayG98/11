c9c65f5bf1c11fbc6ea67883b6093946188eab4b
itayG98 <itai.g98@gmail.com>
2024-10-11 14:05:26 +0300

    initial settings for assignment

---
+++ xy_bits/xy_bits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * @brief Entry point for BLA BLA BLA.
+ *
+ * This function prompts the user for BLA BLA BLA
+ * *
+ * Example usage:
+ * @code
+ *    // Input "lalala"
+ * @endcode
+ */
+int main()
+{
+    unsigned int a, b;
+    printf("We are xy_bits\n");
+    printf("Please insert two decimal unsigned number seperated by spaces.\n");
+    scanf("%u  %u", &a, &b);
+    printf("Press any key to continue...\n");
+    return 1;
+}
