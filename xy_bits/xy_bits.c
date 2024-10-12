e63d079db50e0eecb9eee7de66ae935516badc1d
itayG98 <itai.g98@gmail.com>
2024-10-12 14:07:21 +0300

    Added Method for the y bit check

--- xy_bits/xy_bits.c
+++ xy_bits/xy_bits.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#define SEVEN_DIGIT 7
-#define THIRTEEN_DIGIT 13
+#define SIX_DIGIT 6
+#define TWELVE_DIGIT 12
 #define BOOL_MSG_TRUE "YES"
 #define BOOL_MSG_FALSE "NO"
+
+int print_binary_with_mask(unsigned long toPrint);
+
+void print_is_bit_on(unsigned long num, int index);
+
+int get_bit_value_by_index(unsigned long num, int index);
+
+unsigned long generate_mask_by_shift(int index);
 
 /**
  * @brief Entry point for the program
@@ -19,20 +27,20 @@
  */
 int main()
 {
-    int i;
-    unsigned long x;
+    unsigned long x, y;
     printf("Please insert two unsigned numbers.\n");
-    scanf("%lu", &x);
-    /*scanf("%lu", &y); */
-    printf("x = %d\n", x);
+    scanf("%lu %lu", &x, &y);
+    printf("x = ");
     print_binary_with_mask(x);
-    for (i = 0; i < 32; i++)
-    {
-        printf("bit %2d is = %2d\n", i, get_bit_value_by_index(x, i));
-    }
-    /* printf("y = %d\n", print_binary_with_mask(y));
-     printf("%s", BOOL_MSG_TRUE);*/
-    scanf("%lu");
+    x += generate_mask_by_shift(SIX_DIGIT);
+    printf("Turned on the %d (counted from zero) bit.\n", SIX_DIGIT);
+    printf("x = ");
+    print_binary_with_mask(x);
+    printf("y = ", y);
+    print_binary_with_mask(y);
+    printf("Check if the %d (counted from zero) bit is on.\n", TWELVE_DIGIT);
+    print_is_bit_on(y, TWELVE_DIGIT);
+    scanf("%lu", x);
     return 1;
 }
 
@@ -82,7 +90,7 @@ unsigned long generate_mask_by_shift(int index)
 
 /**
  * @brief Identify a bit value by position.
- * This function takes an unsigned long integer and prints an index's bit
+ * This function takes an unsigned long integer returns the index's bit
  * @param num The unsigned long integer.
  * @param index The index to look for
  * Example usage:
@@ -92,10 +100,27 @@ unsigned long generate_mask_by_shift(int index)
  */
 int get_bit_value_by_index(unsigned long num, int index)
 {
-    int bits_number = sizeof(num) * 8, i = 0;
+    int bits_number = sizeof(num) * 8;
     if (index < 0 || index > bits_number)
     {
         return -1;
     }
     return (num & generate_mask_by_shift(index)) ? 1 : 0;
+}
+
+/**
+ * @brief Prints "YES" if the bit at the specified index of the given number is 1,
+ *        or "NO" if it is 0.
+ * This function uses the index parameter to check the corresponding bit value
+ * in the unsigned long integer 'num'. The result is printed as a string.
+ * @param num The unsigned long integer to check it's bit.
+ * @param index The index of the bit to check (0 based counting)
+ * Example usage:
+ * @code
+ *    print_is_bit_on(5,0);  returns YES
+ * @endcode
+ */
+void print_is_bit_on(unsigned long num, int index)
+{
+    printf("%s", get_bit_value_by_index(num, index) ? BOOL_MSG_TRUE : BOOL_MSG_FALSE);
 }
